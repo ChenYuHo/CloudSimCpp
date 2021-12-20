7906a6473cb57742ec87024a3d74490857b40507
Chen-Yu Ho <chenyu.ho@kaust.edu.sa>
2021-12-20 16:05:17 +0800

    ReadyandGo blocks per jid

--- src/collective_scheduling/ready_and_go.cpp
+++ src/collective_scheduling/ready_and_go.cpp
@@ -12,9 +12,14 @@ simcpp20::event<SIM_UNIT> ReadyAndGo::collective_scheduler(
 simcpp20::event<SIM_UNIT> ReadyAndGo::enqueue(simcpp20::simulation<SIM_UNIT> &sim, Tensor *tensor) {
     queue[tensor->key].push_back(tensor);
     if (queue[tensor->key].size() == tensor->job->num_workers_allocated) {
+        job_locks.try_emplace(tensor->job->id, sim, 1);
+        co_await job_locks.at(tensor->job->id).request();
+        vector<simcpp20::event<SIM_UNIT>> allreduce_events{};
         for (auto &t: queue[tensor->key]) {
-            t->machine->allreduce(sim, t);
+            allreduce_events.push_back(std::move(t->machine->allreduce(sim, t)));
         }
+        co_await sim.all_of(allreduce_events);
+        job_locks.at(tensor->job->id).release();
         queue.erase(tensor->key);
     }
     co_await sim.timeout(0);
--- src/collective_scheduling/ready_and_go.h
+++ src/collective_scheduling/ready_and_go.h
@@ -2,6 +2,7 @@
 #define CLOUDSIMCPP_READY_AND_GO_H
 
 #include "collective_scheduler.h"
+#include "resource.hpp"
 
 
 class ReadyAndGo : public CollectiveScheduler {
@@ -13,6 +14,8 @@ public:
     simcpp20::event<SIM_UNIT> enqueue(simcpp20::simulation<SIM_UNIT> &, Tensor *tensor) override;
 
     simcpp20::event<SIM_UNIT> collective_scheduler(simcpp20::simulation<SIM_UNIT> &sim, Cluster &cluster) override;
+
+    std::unordered_map<unsigned, resource<SIM_UNIT>> job_locks{};
 };
 
 
